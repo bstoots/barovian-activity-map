bb808339a039f2cf08c89020b3097720582e21d3
Brian Stoots <brian@stoots.me>
2021-01-31 15:36:57 -0500

    Add leaflet CSS and JavaScript to index. Add initial leaflet map of Barovia.  Remove default CSS styles.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,47 @@
-// import logo from './logo.svg';
 import './App.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
+
+import { MapContainer, ImageOverlay, Marker } from 'react-leaflet'
+import { CRS } from 'leaflet';
 
 function App() {
+
+  const bounds = [[0, 0], [3225, 5025]];
+  const center = [1612.5, 2512.5];
+
+  // const village_of_barovia = [1240, 3950];
+  const vallaki = [2155, 2020];
+  // const krezk = [2250, 580];
+  // const ravenloft = [1580, 3595];
+
   return (
     <div className="App">
-      <Container fluid>
-        <Navbar variant="dark" bg="dark">
-          <Navbar.Brand href="#">Barovian Activity Map</Navbar.Brand>
-        </Navbar>
-        <Row>
+      <Navbar variant="dark" bg="dark">
+        <Navbar.Brand href="#">Barovian Activity Map</Navbar.Brand>
+      </Navbar>
+
+      {/* cos201.jpg: 5025 x 3225 */}
+      <Container fluid className="p-0">
+        <Row noGutters>
           <Col>
-            <Image src="cos201.jpg" fluid />
+          <MapContainer
+            center={center}
+            zoom={-2}
+            maxZoom={2}
+            minZoom={-2}
+            maxBounds={ bounds }
+            crs={CRS.Simple}
+            id="mapId"
+          >
+            <ImageOverlay
+              url='cos201.jpg'
+              bounds={ bounds }
+            />
+            <Marker position={vallaki}></Marker>
+          </MapContainer>
           </Col>
         </Row>
       </Container>
